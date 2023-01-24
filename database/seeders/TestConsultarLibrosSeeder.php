a0bff828dd65298df8a662c7bade6d8d87b0e153
angeles <angelesfernandez2810@gmail.com>
2023-01-24 21:56:39 +0100

    Creating tests

--- database/seeders/TestConsultarLibrosSeeder.php
+++ database/seeders/TestConsultarLibrosSeeder.php
@@ -24,18 +24,21 @@ class TestConsultarLibrosSeeder extends Seeder
         DB::table('libros')->insert([
             'titulo' => 'Un guijarro en el cielo',
             'f_publicacion' => '19/01/1950',
+            'sinopsis' => 'Erase una vez una sinopsis muy corta sobre UN GUIJARRO EN EL CIELO. FIN',
             'id_autor' => '1',
         ]);
 
         DB::table('libros')->insert([
             'titulo' => 'Fundación',
             'f_publicacion' => '01/06/1951',
+            'sinopsis' => 'Erase una vez una sinopsis muy corta sobre FUNDACIÓN. FIN',
             'id_autor' => '1',
         ]);
 
         DB::table('libros')->insert([
             'titulo' => 'Yo, robot',
             'f_publicacion' => '02/12/1950',
+            'sinopsis' => 'Erase una vez una sinopsis muy corta sobre YO, ROBOT. FIN',
             'id_autor' => '1',
         ]);
     }
