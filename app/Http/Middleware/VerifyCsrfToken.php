15de061d2385a520c4ae17953570482864a31ea7
angeles <angelesfernandez2810@gmail.com>
2023-01-28 17:41:56 +0100

    Excluyendo uso de CSRF token en servidor

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'http://localhost:8000/**'
+        'http://localhost:8000/**',
+        'http://angeles-fernandez-gomez-tarea-6.duckdns.org/**'
     ];
 }
